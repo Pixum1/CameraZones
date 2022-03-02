688c3a8b91eb949f5ed9546b963de75aae495bea
Pixum <pixum2001@gmail.com>
2022-03-02 16:56:05 +0100

    Added Mesh to Camera Zone

--- Assets/Scripts/ZoneManager.cs
+++ Assets/Scripts/ZoneManager.cs
@@ -27,6 +27,33 @@ public class ZoneManager : MonoBehaviour
         zone.GetComponent<CameraZone>().zoneManager = this;
         zone.GetComponent<CameraZone>().cameraOrthographicSize = defaultCamSize;
         zone.GetComponent<CameraZone>().index = zones.Count - 1;
+		zone.AddComponent<MeshRenderer>().materials = new Material[0];
+		CreateCube(zone);
         return zone;
     }
+
+	private void CreateCube(GameObject _obj)
+	{
+		Vector3[] vertices = {
+			new Vector2 (-.5f, -.5f),
+			new Vector2 (.5f, -.5f),
+			new Vector2 (.5f, .5f),
+			new Vector2 (-.5f, .5f),
+		};
+
+		int[] triangles = {
+			0, 2, 1, //face front
+			0, 3, 2,
+		};
+
+        Mesh s = new Mesh();
+
+		_obj.AddComponent<MeshFilter>();
+		_obj.GetComponent<MeshFilter>().sharedMesh = s;
+		s.Clear();
+		s.vertices = vertices;
+		s.triangles = triangles;
+		s.Optimize();
+		s.RecalculateNormals();
+	}
 }
